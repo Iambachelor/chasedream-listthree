76c5b59e8cf7e840b41088606269573b36f4754e
15083267362@163.com <15083267362@163.com>
2020-12-05 22:25:42 +0800

    2020-12-05 mybatis入门

---
+++ src/main/java/cn/dreamchase/chasedream_listthree/App.java
@@ -0,0 +1,13 @@
+package cn.dreamchase.chasedream_listthree;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
